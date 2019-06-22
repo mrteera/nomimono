fa444f0a6747ae8295e123c569f5b5e5eb686c47
Visutr Boonnateephisit <visutr@Visutrs-MacBook-Pro.local>
2019-06-22 12:44:44 +0700

    Added firebase dependencies

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAyd6W8vP6zOUPdpg6vvVP4S7xhoXyGzfA",
+    authDomain: "nomimono-d320d.firebaseapp.com",
+    databaseURL: "https://nomimono-d320d.firebaseio.com",
+    projectId: "nomimono-d320d",
+    storageBucket: "",
+    messagingSenderId: "458497146563",
+    appId: "1:458497146563:web:7fc4b71d1920ac73"
+  }
 };
 
 /*
